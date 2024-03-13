57335b167c5bc4d4d015961e2947cedfc02e2c88
bhanurz <aswanggab@gmail.com>
2024-03-13 13:09:42 +0700

    feat: add sequelize configuration

---
+++ src/utils/sequelize.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize({
+  dialect: "postgres",
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+module.exports = sequelize;
