afdac8e014312b6bf38ae60144aeaa1fb868f599
bhanurz <aswanggab@gmail.com>
2024-03-23 19:58:40 +0700

    fix: auth requirements

--- src/middlewares/validator/auth.requirements.js
+++ src/middlewares/validator/auth.requirements.js
@@ -2,15 +2,15 @@ const { body } = require("express-validator");
 
 const requirements = {
   login: [
-    body("email").isEmail(),
-    body("password").isString().isLength({ min: 8 }),
+    body("email").notEmpty().isEmail(),
+    body("password").notEmpty().isString().isLength({ min: 8 }),
   ],
   registerCustomer: [
-    body("fullname").isString().notEmpty(),
-    body("email").isEmail(),
-    body("password").isString().isLength({ min: 8 }),
-    body("telephone").isMobilePhone().notEmpty(),
-    body("address").isString().notEmpty(),
+    body("fullname").notEmpty().isString(),
+    body("email").notEmpty().isEmail(),
+    body("password").notEmpty().isString().isLength({ min: 8 }),
+    body("telephone").notEmpty().isMobilePhone("id-ID"),
+    body("address").notEmpty().isString(),
   ],
 };
 
