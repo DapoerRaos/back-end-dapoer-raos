56d04a2793a2c5ea3eba161c3785a2d469104f11
bhanurz <aswanggab@gmail.com>
2024-03-23 19:47:16 +0700

    feat: setup migrate tables and seed data for db

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,15 +1,23 @@
-const { userModel, customerModel } = require("../models");
-const { userSeeders, customerSeeders } = require("../seeders");
+const {
+  userModel,
+  customerModel,
+  productModel,
+  categoryModel,
+} = require("../models");
+const { userSeeders, customerSeeders, categorySeeders } = require("../seeders");
 const logger = require("../utils/logger");
 
 async function migrateTables() {
   await userModel.sync({ force: true });
   await customerModel.sync({ force: true });
+  await categoryModel.sync({ force: true });
+  await productModel.sync({ force: true });
 }
 
 async function seedData() {
   await userSeeders();
   await customerSeeders();
+  await categorySeeders();
 }
 
 async function syncDatabase() {
