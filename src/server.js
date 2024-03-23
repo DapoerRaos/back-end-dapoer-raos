15b4ac981173300f8f571f7d20012810152333bb
bhanurz <aswanggab@gmail.com>
2024-03-23 19:26:23 +0700

    feat: add cors setup for server and express static file

--- src/server.js
+++ src/server.js
@@ -3,10 +3,18 @@ const { API } = require("./config");
 const routes = require("./routes");
 const { morganMiddleware, errorHandler } = require("./middlewares");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 function createServer() {
   const app = express();
 
+  const corsOption = {
+    origin: "*",
+    credentials: true,
+  };
+
+  app.use(cors(corsOption));
+  app.use(express.static("uploads"));
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
   app.use(cookieParser());
