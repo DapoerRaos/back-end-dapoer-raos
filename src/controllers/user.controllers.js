756ae9182d352aef40870877eee98e6117e2f555
bhanurz <aswanggab@gmail.com>
2024-03-24 12:03:38 +0700

    feat: add user controller

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,29 @@
+const { userServices } = require("../services");
+
+async function getUserById(req, res) {
+  try {
+    const result = await userServices.getUserById(req.userData.id);
+    res.status(200).json({
+      status: "Success",
+      data: result,
+    });
+  } catch (err) {
+    if (err.message === "Not Found") {
+      logger.error({ status: 404, error: err });
+      res.status(404).json({
+        status: "Failed",
+        message: "User Tidak Ditemukan",
+      });
+    } else {
+      logger.error({ status: 500, error: err });
+      res.status(500).json({
+        status: "Failed",
+        message: "Internal server error",
+      });
+    }
+  }
+}
+
+module.exports = {
+  getUserById,
+};
