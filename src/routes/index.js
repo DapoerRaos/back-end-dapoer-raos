a45af009c3a9fef1a0cbb2c585d42c59ff8c3c29
bhanurz <aswanggab@gmail.com>
2024-03-24 12:00:07 +0700

    feat: add category, customer, product, user routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,16 @@
 const { Router } = require("express");
-const userRoutes = require("./users.routes");
-const customerRoutes = require("./customers.routes");
+const userRoutes = require("./user.routes");
+const customerRoutes = require("./customer.routes");
 const authRoutes = require("./auth.routes");
+const productRoutes = require("./product.routes");
+const categoryRoutes = require("./category.routes");
 
 const router = Router();
 
+router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/customers", customerRoutes);
-router.use("/auth", authRoutes);
+router.use("/products", productRoutes);
+router.use("/categories", categoryRoutes);
 
 module.exports = router;
