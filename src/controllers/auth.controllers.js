04b62d448404ebcba39f4cc80debb642242d77e3
bhanurz <aswanggab@gmail.com>
2024-03-24 12:02:30 +0700

    fix: auth response controllers

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -4,12 +4,16 @@ const { authServices } = require("../services");
 async function login(req, res) {
   try {
     const token = await authServices.login(req.body);
-    res.cookie("token", token);
-    res.status(200).json({
-      status: "Success",
-      message: "Successfully logged in",
-      token,
-    });
+    res
+      .status(200)
+      .cookie("token", token, {
+        httpOnly: true,
+      })
+      .json({
+        status: "Success",
+        message: "Login Berhasil",
+        token,
+      });
   } catch (err) {
     handleAuthError(err, res);
   }
@@ -27,7 +31,7 @@ async function registerCustomer(req, res) {
     });
     res.status(201).json({
       status: "Success",
-      message: "User successfully registered",
+      message: "Registrasi Berhasil",
     });
   } catch (err) {
     handleAuthError(err, res);
@@ -39,25 +43,31 @@ function handleAuthError(err, res) {
     logger.error({ status: 401, error: err });
     res.status(401).json({
       status: "Failed",
-      message: "Incorrect Email or Password",
+      message: "Email atau Password Salah",
     });
   } else if (err.message === "All fields are required") {
-    logger.error({ status: 422, error: err });
-    res.status(422).json({
+    logger.error({ status: 409, error: err });
+    res.status(409).json({
       status: "Failed",
-      message: "All fields are required",
+      message: "Semua Kolom Wajib Diisi",
     });
   } else if (err.message === "Email already exists") {
-    logger.error({ status: 422, error: err });
-    res.status(422).json({
+    logger.error({ status: 409, error: err });
+    res.status(409).json({
       status: "Failed",
-      message: "Email already exists",
+      message: "Email Sudah Digunakan",
+    });
+  } else if (err.message === "Phone Number already used") {
+    logger.error({ status: 409, error: err });
+    res.status(409).json({
+      status: "Failed",
+      message: "No Hp Sudah Digunakan",
     });
   } else {
     logger.error({ status: 500, error: err });
     res.status(500).json({
       status: "Failed",
-      message: "Internal Server Error",
+      message: "Terjadi Error Pada Server",
     });
   }
 }
