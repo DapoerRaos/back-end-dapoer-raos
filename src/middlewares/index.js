0496d527e3be674fda66e6c8e688643dc363d306
bhanurz <aswanggab@gmail.com>
2024-03-24 11:57:00 +0700

    feat: add multer middleware for file handling

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,10 +2,12 @@ const morganMiddleware = require("./morgan.middleware");
 const errorHandler = require("./errorHandler.middleware");
 const validator = require("./validator");
 const authMiddleware = require("./auth.middleware");
+const multerMiddleware = require("./multer.middleware");
 
 module.exports = {
   morganMiddleware,
   errorHandler,
   validator,
   authMiddleware,
+  multerMiddleware,
 };
---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,26 @@
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const uploadDir = path.join(__dirname, "../../uploads/products");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const diskStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/products");
+  },
+  filename: (req, file, cb) => {
+    const fileName = file.originalname.toLowerCase().split(" ").join("-");
+    cb(null, Date.now() + "-" + fileName);
+  },
+});
+
+const multerMiddleware = multer({
+  storage: diskStorage,
+  limits: { fileSize: 5000000 },
+}).single("image");
+
+module.exports = multerMiddleware;
