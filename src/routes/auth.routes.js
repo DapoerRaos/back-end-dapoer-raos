ce6d5076b14ec9c94a80933346b44cd6508111fe
bhanurz <aswanggab@gmail.com>
2024-04-01 22:03:01 +0700

    feat: add logout routes for user

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,9 @@
 const { Router } = require("express");
-const { validator } = require("../middlewares");
+const { validator, authMiddleware } = require("../middlewares");
 const { authControllers } = require("../controllers");
 
 const router = Router();
 const { validate, requirements } = validator;
-
-router
-  .route("/login")
-  .post([validate(requirements.login)], authControllers.login);
 
 router
   .route("/register")
@@ -15,5 +11,13 @@ router
     [validate(requirements.registerCustomer)],
     authControllers.registerCustomer
   );
+
+router
+  .route("/login")
+  .post([validate(requirements.login)], authControllers.login);
+
+router
+  .route("/logout")
+  .post([authMiddleware.authenticate], authControllers.logout);
 
 module.exports = router;
