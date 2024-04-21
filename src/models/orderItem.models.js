fa64107a16e65b4c44224311ac1b566425eb5784
bhanurz <aswanggab@gmail.com>
2024-04-21 13:47:48 +0700

    feat: add order item model

---
+++ src/models/orderItem.models.js
@@ -0,0 +1,50 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../utils/sequelize");
+const Order = require("./order.models");
+const Product = require("./product.models");
+
+const OrderItem = sequelize.define(
+  "OrderItem",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    order_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: Order,
+        key: "id",
+      },
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Product,
+        key: "id",
+      },
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    total_price: {
+      type: DataTypes.DOUBLE(10, 2),
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "order_items",
+    freezeTableName: true,
+    timestamps: true,
+  }
+);
+
+OrderItem.belongsTo(Product, { foreignKey: "product_id" });
+OrderItem.belongsTo(Order, { foreignKey: "order_id" });
+Order.hasMany(OrderItem, { foreignKey: "order_id" });
+
+module.exports = OrderItem;
