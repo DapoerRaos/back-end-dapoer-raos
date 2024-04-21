37bc88024e92b00ad036a96bd42899e7b15471b7
bhanurz <aswanggab@gmail.com>
2024-04-21 13:40:40 +0700

    feat: add midtrans server key configuration

--- src/config/index.js
+++ src/config/index.js
@@ -5,6 +5,7 @@ const config = {
   PORT: parseInt(process.env.PORT) || 3001,
   API: process.env.API_PATH || "api/v1",
   SECRET_KEY: process.env.SECRET_KEY,
+  MIDTRANS_SERVER_KEY: process.env.MIDTRANS_SERVER_KEY,
 };
 
 module.exports = config;
