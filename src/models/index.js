854b3ab4665b3199cfa5a6e279f2df2e2a43e03b
bhanurz <aswanggab@gmail.com>
2024-04-21 13:49:33 +0700

    featL add few model import module

--- src/models/index.js
+++ src/models/index.js
@@ -2,10 +2,18 @@ const userModel = require("./user.models");
 const customerModel = require("./customer.models");
 const categoryModel = require("./category.models");
 const productModel = require("./product.models");
+const cartModel = require("./cart.models");
+const cartDetailModel = require("./cartDetail.models");
+const orderModel = require("./order.models");
+const orderItemModel = require("./orderItem.models");
 
 module.exports = {
   userModel,
   customerModel,
   categoryModel,
   productModel,
+  cartModel,
+  cartDetailModel,
+  orderModel,
+  orderItemModel,
 };
