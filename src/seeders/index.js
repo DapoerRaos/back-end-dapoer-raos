f78728585a7e7a8fdf6610e334f0acbd8fcbc31f
bhanurz <aswanggab@gmail.com>
2024-04-21 13:52:20 +0700

    feat: add import file for product and cart seeders

--- src/seeders/index.js
+++ src/seeders/index.js
@@ -1,9 +1,13 @@
 const userSeeders = require("./user.seeders");
 const customerSeeders = require("./customer.seeders");
 const categorySeeders = require("./category.seeders");
+const productSeeders = require("./product.seeders");
+const cartSeeders = require("./cart.seeders");
 
 module.exports = {
   userSeeders,
   customerSeeders,
   categorySeeders,
+  productSeeders,
+  cartSeeders,
 };
