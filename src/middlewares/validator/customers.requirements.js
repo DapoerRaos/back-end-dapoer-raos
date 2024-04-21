d5e29ffeadf6b222610d7c9829fe508d399d532c
bhanurz <aswanggab@gmail.com>
2024-04-21 13:50:13 +0700

    fix: requirement req body email to custom validator

--- src/middlewares/validator/customers.requirements.js
+++ src/middlewares/validator/customers.requirements.js
@@ -2,8 +2,13 @@ const { body, param } = require("express-validator");
 
 const requirements = {
   updateCustomer: [
-    body("email").optional().isEmail(),
-    body("password").optional().isString().isLength({ min: 8 }),
+    body("email")
+      .optional()
+      .custom((value, { req }) => {
+        if (value === "") return true;
+        return req.check("email").isEmail();
+      }),
+    body("password").optional().isString(),
     body("fullname").optional().isString(),
     body("telephone").optional().isMobilePhone("id-ID"),
     body("address").optional().isString(),
