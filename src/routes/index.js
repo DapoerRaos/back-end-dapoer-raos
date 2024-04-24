2cb1c0439d837b28bf480a23e2620242f4da67c7
bhanurz <aswanggab@gmail.com>
2024-04-24 22:05:41 +0700

    feat: add few import file routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,10 @@ const customerRoutes = require("./customer.routes");
 const authRoutes = require("./auth.routes");
 const productRoutes = require("./product.routes");
 const categoryRoutes = require("./category.routes");
+const cartRoutes = require("./cart.routes");
+const cartDetailRoutes = require("./cartDetail.routes");
+const orderRoutes = require("./order.routes");
+const orderItemRoutes = require("./orderItem.routes");
 
 const router = Router();
 
@@ -12,5 +16,9 @@ router.use("/users", userRoutes);
 router.use("/customers", customerRoutes);
 router.use("/products", productRoutes);
 router.use("/categories", categoryRoutes);
+router.use("/carts", cartRoutes);
+router.use("/carts/detail", cartDetailRoutes);
+router.use("/orders", orderRoutes);
+router.use("/orders/items", orderItemRoutes);
 
 module.exports = router;
