ed543adfea77de03be0adcdc938bf41cda2312fb
bhanurz <aswanggab@gmail.com>
2024-04-24 21:53:59 +0700

    feat: add cart, cartDetail, order, orderItem model migrate table and seed

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,8 +3,18 @@ const {
   customerModel,
   productModel,
   categoryModel,
+  cartModel,
+  cartDetailModel,
+  orderModel,
+  orderItemModel,
 } = require("../models");
-const { userSeeders, customerSeeders, categorySeeders } = require("../seeders");
+const {
+  userSeeders,
+  customerSeeders,
+  categorySeeders,
+  productSeeders,
+  cartSeeders,
+} = require("../seeders");
 const logger = require("../utils/logger");
 
 async function migrateTables() {
@@ -12,12 +22,18 @@ async function migrateTables() {
   await customerModel.sync({ force: true });
   await categoryModel.sync({ force: true });
   await productModel.sync({ force: true });
+  await cartModel.sync({ force: true });
+  await cartDetailModel.sync({ force: true });
+  await orderModel.sync({ force: true });
+  await orderItemModel.sync({ force: true });
 }
 
 async function seedData() {
   await userSeeders();
   await customerSeeders();
   await categorySeeders();
+  await productSeeders();
+  await cartSeeders();
 }
 
 async function syncDatabase() {
