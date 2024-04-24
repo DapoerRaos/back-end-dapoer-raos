f721a52548c4398cd2e0854d574867320f291190
bhanurz <aswanggab@gmail.com>
2024-04-24 21:58:51 +0700

    refactor&feat: add city and postal code field, change dummy data

--- src/seeders/customer.seeders.js
+++ src/seeders/customer.seeders.js
@@ -3,19 +3,49 @@ const logger = require("../utils/logger");
 
 const customers = [];
 
-const createCustomers = (user_id, fullname, telephone, address) => {
+const createCustomers = (
+  user_id,
+  fullname,
+  telephone,
+  address,
+  city,
+  postal_code
+) => {
   const customer = {
     user_id,
     fullname,
     telephone,
     address,
+    city,
+    postal_code,
   };
   customers.push(customer);
 };
 
-createCustomers(2, "Customer 1", "081345678910", "Address 1");
-createCustomers(3, "Customer 2", "081245678910", "Address 2");
-createCustomers(4, "Customer 3", "081234567893", "Address 3");
+createCustomers(
+  2,
+  "Aswangga Bhanu Rizqullah",
+  "081320909411",
+  "Griya Pamulang 2 Blok C3/10 RT02,RW20 Pondok Benda",
+  "Tangerang Selatan",
+  "15416"
+);
+createCustomers(
+  3,
+  "Leandra Chika Gayatri",
+  "085973987950",
+  "Pamulang Permai Raya Blok A2/50 RT07,RW09 Pamulang Timur",
+  "Tangerang Selatan",
+  "15417"
+);
+createCustomers(
+  4,
+  "Raul Aksal",
+  "085697963945",
+  "Jalan Cendrawasih No.8 RT.1/RW.1 Sawah Lama, Ciputat",
+  "Tangerang Selatan",
+  "15415"
+);
 
 const seedCustomers = async () => {
   try {
