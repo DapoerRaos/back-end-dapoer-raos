018027eb93ca46cdd4546ebabc10dce793c245f7
bhanurz <aswanggab@gmail.com>
2024-04-28 22:56:07 +0700

    fix: update customer fuction (email, password, and telephone validation)

--- src/services/customer.services.js
+++ src/services/customer.services.js
@@ -21,40 +21,65 @@ async function getCustomerById(id) {
   return customer;
 }
 
-async function updateCustomerById(id, data) {
-  const { email, password, fullname, telephone, address } = data;
+async function getCustomerDetailById(id) {
+  const customer = await customerRepositories.getCustomerDetailById(id);
 
-  const isCustomerExists = await customerRepositories.getCustomerById(id);
-  if (!isCustomerExists) {
+  if (!customer) {
     throw new Error("Not Found");
   }
 
-  if (email) {
-    const existingUserWithEmail = await userRepositories.getUserByEmail(email);
-    if (
-      existingUserWithEmail &&
-      existingUserWithEmail.id !== isCustomerExists.id
-    ) {
-      throw new Error("Email already exists");
+  return customer;
+}
+
+async function updateCustomerById(id, data) {
+  const { email, password, fullname, telephone, address, city, postal_code } =
+    data;
+
+  const user = await customerRepositories.getCustomerById(id);
+
+  if (!user) {
+    throw new Error("Not Found");
+  }
+
+  let existEmail;
+  if (email && email !== "") {
+    existEmail = await userRepositories.getUserByEmail(email);
+  }
+
+  if (existEmail && user.email != existEmail.email) {
+    throw new Error("Email already exists");
+  }
+
+  let hashPassword;
+  if (password && password !== "") {
+    hashPassword = await bcrypt.hash(password, 10);
+  }
+
+  let parshedTelephone;
+  if (telephone && telephone !== "") {
+    parshedTelephone = telephone.replace(/^\+62/, "0");
+    const checkTelephone = await customerRepositories.getCustomerByTelephone(
+      parshedTelephone
+    );
+    if (checkTelephone) {
+      throw new Error("Phone Number already used");
     }
   }
 
-  let hashedPassword;
-  if (password) {
-    hashedPassword = await bcrypt.hash(password, 10);
-  }
-
   await customerRepositories.updateCustomerById(id, {
-    email: email || isCustomerExists.email,
-    password: hashedPassword || isCustomerExists.password,
-    fullname: fullname || isCustomerExists.fullname,
-    telephone: telephone || isCustomerExists.telephone,
-    address: address || isCustomerExists.address,
+    email: email === "" ? user.email : email,
+    password: hashPassword,
+    fullname: fullname === "" ? user.fullname : fullname,
+    telephone: parshedTelephone || user.telephone,
+    address: address === "" ? user.address : address,
+    city: city === "" ? user.city : city,
+    postal_code: postal_code === "" ? user.postal_code : postal_code,
   });
 }
 
 module.exports = {
   getCustomers,
   getCustomerById,
+  getCustomerDetailById,
   updateCustomerById,
 };
