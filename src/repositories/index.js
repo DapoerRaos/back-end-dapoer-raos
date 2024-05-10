6805400b294132370ac1a7481fb6f3b5e0868501
bhanurz <aswanggab@gmail.com>
2024-05-10 13:29:22 +0700

    feat: add import file for cart and order

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -2,10 +2,18 @@ const userRepositories = require("./user.repositories");
 const customerRepositories = require("./customer.repositories");
 const productRepositories = require("./product.repositories");
 const categoryRepositories = require("./category.repositories");
+const cartRepositories = require("./cart.repositories");
+const cartDetailRepositories = require("./cartDetail.repositories");
+const orderRepositories = require("./order.repositories");
+const orderItemRepositories = require("./orderItem.repositories");
 
 module.exports = {
   userRepositories,
   customerRepositories,
   productRepositories,
   categoryRepositories,
+  cartRepositories,
+  cartDetailRepositories,
+  orderRepositories,
+  orderItemRepositories,
 };
