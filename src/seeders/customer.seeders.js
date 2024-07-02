f4580cff80e7ae93968b3a4618599d5723fcae02
bhanurz <aswanggab@gmail.com>
2024-07-02 15:30:53 +0700

    fix: remove unused field

--- src/seeders/customer.seeders.js
+++ src/seeders/customer.seeders.js
@@ -3,49 +3,18 @@ const logger = require("../utils/logger");
 
 const customers = [];
 
-const createCustomers = (
-  user_id,
-  fullname,
-  telephone,
-  address,
-  city,
-  postal_code
-) => {
+const createCustomers = (user_id, fullname, telephone) => {
   const customer = {
     user_id,
     fullname,
     telephone,
-    address,
-    city,
-    postal_code,
   };
   customers.push(customer);
 };
 
-createCustomers(
-  2,
-  "Aswangga Bhanu Rizqullah",
-  "081320909411",
-  "Griya Pamulang 2 Blok C3/10 RT02,RW20 Pondok Benda",
-  "Tangerang Selatan",
-  "15416"
-);
-createCustomers(
-  3,
-  "Leandra Chika Gayatri",
-  "085973987950",
-  "Pamulang Permai Raya Blok A2/50 RT07,RW09 Pamulang Timur",
-  "Tangerang Selatan",
-  "15417"
-);
-createCustomers(
-  4,
-  "Raul Aksal",
-  "085697963945",
-  "Jalan Cendrawasih No.8 RT.1/RW.1 Sawah Lama, Ciputat",
-  "Tangerang Selatan",
-  "15415"
-);
+createCustomers(2, "Aswangga Bhanu Rizqullah", "081320909411");
+createCustomers(3, "Leandra Chika Gayatri", "085973987950");
+createCustomers(4, "Raul Aksal", "085697963945");
 
 const seedCustomers = async () => {
   try {
