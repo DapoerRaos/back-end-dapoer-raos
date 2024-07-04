dd19d597e7dbabbb374bd348eff1cc74fc81dfb2
bhanurz <aswanggab@gmail.com>
2024-07-04 16:54:00 +0700

    feat&fix: add order by date service and send email after update status service

--- src/services/order.services.js
+++ src/services/order.services.js
@@ -1,14 +1,14 @@
 const { orderRepositories } = require("../repositories");
+const nodemailer = require("nodemailer");
+const { APP_MAIL, APP_PASSWORD } = require("../config");
 
 async function createOrder(data) {
   const {
     id,
     customer_id,
+    shipping_id,
     total_price,
     status,
-    shipping_status,
-    shipping_type,
-    shipping_cost,
     payment_method,
     va_number,
     bank,
@@ -18,10 +18,9 @@ async function createOrder(data) {
   if (
     !id ||
     !customer_id ||
+    !shipping_id ||
     !total_price ||
     !status ||
-    !shipping_status ||
-    !shipping_type ||
     !payment_method ||
     !payment_date
   ) {
@@ -31,11 +30,9 @@ async function createOrder(data) {
   return await orderRepositories.createOrder({
     id,
     customer_id,
+    shipping_id,
     total_price,
     status,
-    shipping_status,
-    shipping_type,
-    shipping_cost,
     payment_method,
     va_number,
     bank,
@@ -45,7 +42,10 @@ async function createOrder(data) {
 
 async function getOrders({ page = 1, keyword = "" }) {
   const limit = 10;
-  const result = await orderRepositories.getOrders({ page, keyword });
+  const result = await orderRepositories.getOrders({
+    page,
+    keyword,
+  });
 
   if (result.length === 0) {
     throw new Error("Order is Empty!");
@@ -60,6 +60,22 @@ async function getOrders({ page = 1, keyword = "" }) {
       total: result.count,
       totalPages: totalPages,
     },
+    order_list: result.rows,
+  };
+}
+
+async function getOrderByDate({ keyword = "", startDate, endDate }) {
+  const result = await orderRepositories.getOrderByDate({
+    keyword,
+    startDate,
+    endDate,
+  });
+
+  if (result.length === 0) {
+    throw new Error("Order is Empty!");
+  }
+
+  return {
     order_list: result.rows,
   };
 }
@@ -117,13 +133,69 @@ async function getOrderByCustomerId(customer_id, { page = 1, keyword = "" }) {
   };
 }
 
-async function updateOrderStatus(id, newStatus, shipping_status) {
+async function updateOrderStatus(id, shipping_id, newStatus, shipping_status) {
   if (!id) {
     throw new Error("Invalid Id");
   }
 
+  const orderDetail = await orderRepositories.getOrderStatusById({
+    transaction_id: id,
+  });
+
+  const email =
+    orderDetail.dataValues.Customer.dataValues.User.dataValues.email;
+
+  if (shipping_status) {
+    const shippingStatusMessage =
+      shipping_status === "Menunggu Pembayaran"
+        ? "Sedang Menunggu Pembayaran"
+        : shipping_status === "Barang di Kemas"
+        ? "Barang sedang dikemas"
+        : shipping_status === "Barang Siap Diambil"
+        ? "Barang siap diambil"
+        : shipping_status === "Barang Dikirim"
+        ? "Sudah dikirim, sedang dalam perjalanan"
+        : shipping_status === "Barang Telah Diambil"
+        ? "Barang Sudah diambil"
+        : shipping_status === "Barang Telah Diterima"
+        ? "Barang sudah sampai tujuan, barang telah diterima"
+        : "";
+
+    const htmlMessage = `
+  <div style="font-family: Arial, sans-serif; line-height: 1.6;">
+    <h2 style="color: #2e6c80;">Informasi Pesanan Anda</h2>
+    <p>Halo,</p>
+    <p>Pesanan dengan ID <strong>${id}</strong> saat ini:</p>
+    <p style="font-size: 1.1em; font-weight: bold; color: #feab3b;">${shippingStatusMessage}</p>
+    <p>Terima kasih telah berbelanja di Dapoer Raos. Kami akan terus memperbarui status pengiriman Anda.</p>
+    <br>
+    <p>Salam,</p>
+    <p style="font-weight: bold;">Dapoer Raos</p>
+  </div>
+`;
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: APP_MAIL,
+        pass: APP_PASSWORD,
+      },
+    });
+
+    await transporter.sendMail({
+      from: '"Dapoer Raos" <no-reply@dapoerraos.com>',
+      to: email,
+      subject: `Informasi Pesanan dengan ID ${id}`,
+      html: htmlMessage,
+    });
+  }
+
   return await orderRepositories.updateOrderStatus(
     id,
+    shipping_id,
     newStatus,
     shipping_status
   );
@@ -132,6 +204,7 @@ async function updateOrderStatus(id, newStatus, shipping_status) {
 module.exports = {
   createOrder,
   getOrders,
+  getOrderByDate,
   getOrderByStatus,
   getOrderStatusById,
   getOrderByCustomerId,
